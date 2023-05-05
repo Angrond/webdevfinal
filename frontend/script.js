73554abe237f6d6e45910176dd39c4fe1fe775c5
Botond Szabó <szbotond2002@gmail.com>
2023-05-05 10:54:53 +0200

    javascript igazitasok, szures elotti allapot

--- frontend/script.js
+++ frontend/script.js
@@ -4,6 +4,10 @@ let sajat_obj ={
 let objnyelvek ={
     nyelvek : [],
 }
+var lastid=-1;
+function tabla(){
+    document.getElementById("bg").innerHTML='<table class="table table-striped-columns" id="tablazat"><thead><h2>Projectek:</h2><hr></thead><tbody id="tabla"><tr><td><h5>Project Neve</h5></td><td><div class="dropdown"><button class="dropbtn" id="nobtn"><h5>Programozási nyelvek</h5></button><div class="dropdown-content" id="ddm"></div></div></td><td><h5>Link</h5></td></tr></tbody></table>'
+}
 function nyelvek(i)
 {
     let lista="";
@@ -14,22 +18,17 @@ function nyelvek(i)
 }
 function apitolt()
 {
-    try { 
-        for(let i=0; i<sajat_obj.project.length; i++)
-        {
-        const element=document.getElementById("sor");
-        element.remove();
-        }
-        } catch (error) {}
+    tabla()
         sajat_obj ={
         project : [],
-    }
+        }
     fetch("http://127.0.0.1:8000/api/projects/")
     .then(data1 => data1.json())
     .then(adat1 => {
         for (let i=0; i<adat1.length; i++){
             sajat_obj.project.push(adat1[i]);
         }
+        
         for(let i=0; i<sajat_obj.project.length; i++)
         {
             var a=""
@@ -40,7 +39,9 @@ function apitolt()
 }
 function feltolt()
 {
-    closedesc();
+    objnyelvek ={
+        nyelvek : [],
+    }
     apitolt();
     fetch("http://127.0.0.1:8000/api/languages/")
     .then(data => data.json())
@@ -48,29 +49,49 @@ function feltolt()
         for (let i=0; i<adat.length; i++){
             objnyelvek.nyelvek.push(adat[i]);
             }
-        for(let i=0; i<objnyelvek.length; i++)
+        document.getElementById("ddm").innerHTML='<button onclick="feltolt()">Mind</button>';
+        for(let i=0; i<objnyelvek.nyelvek.length; i++)
         {
-            document.getElementById("dropdown-content").innerHTML+="<button onclick='keres()'>"+objnyelvek.nyelvek[i].name+"</button>"
+            
+            document.getElementById("ddm").innerHTML+="<button onclick='keres("+objnyelvek.nyelvek[i].name+")'>"+objnyelvek.nyelvek[i].name+"</button>"
         };    
-        console.log(objnyelvek)
+        
     })
 }
-function keres(){
-
+function keres(nyelv){
+sajat_obj.project.forEach(element => {
+    if (element.languages.Includes(nyelv)) {
+        
+    }
+});
 }
 function description(id){
-    if(document.getElementById("desc")){
-        closedesc();
+    if (id==lastid) {
+        if(document.getElementById("desc")){
+            closedesc();
+        }
+        else{
+        let tableRef = document.getElementById("tabla");
+        let newRow = tableRef.insertRow(id+2);
+        newRow.id="desc";
+        let newCell = newRow.insertCell(0);
+        newCell.colSpan=3;
+        let newText = document.createTextNode(sajat_obj.project[id].description);
+        newCell.appendChild(newText);
+        lastid=id;
+        }
     }
     else{
-    let tableRef = document.getElementById("tabla");
-    let newRow = tableRef.insertRow(id+2);
-    newRow.id="desc";
-    let newCell = newRow.insertCell(0);
-    newCell.colSpan=3;
-    let newText = document.createTextNode(sajat_obj.project[id].description);
-    newCell.appendChild(newText);
-    }
+        closedesc();
+        let tableRef = document.getElementById("tabla");
+        let newRow = tableRef.insertRow(id+2);
+        newRow.id="desc";
+        let newCell = newRow.insertCell(0);
+        newCell.colSpan=3;
+        let newText = document.createTextNode(sajat_obj.project[id].description);
+        newCell.appendChild(newText);
+        lastid=id;
+    }    
 }
 function closedesc(){
     try {
@@ -78,4 +99,5 @@ function closedesc(){
         var table = row.parentNode;
         table.deleteRow(row.rowIndex);
     } catch (error) {} 
+    lastid=-1;
 }
